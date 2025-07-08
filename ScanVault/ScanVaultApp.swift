b046adcf1b7b7bfa21630c31fa46d20565cdc566
dhruv <dhruvsurani122@gmail.com>
2025-07-07 21:11:32 -0500

    Initial Commit

---
+++ ScanVault/ScanVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanVaultApp.swift
+//  ScanVault
+//
+//  Created by DhruvSurani on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ScanVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScanVaultTests/ScanVaultTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScanVaultTests.swift
+//  ScanVaultTests
+//
+//  Created by DhruvSurani on 7/7/25.
+//
+
+import Testing
+
+struct ScanVaultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
